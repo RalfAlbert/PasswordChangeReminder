34d57bead600ef73c95410af93176fa5cf2a65cf
Ralf Albert <neun12@gmail.com>
2013-11-23 15:58:38 +0100

    adjust javascript to work in frontend

--- scripts/pwcr_backend.js
+++ scripts/pwcr_backend.js
@@ -4,10 +4,34 @@
  */
 jQuery( document ).ready(
 	function($){
+		/*
+		 * guessing the url to admin-ajax.php. this is used if no object was created by the plugin
+		 * and we are in frontend where no 'ajaxurl' is available.
+		 */
+		var ajax_url = '';
+		var guessed_ajaxurl = location.protocol + '//' + location.host + location.pathname + 'wp-admin/admin-ajax.php';
+
+		if( typeof ajaxurl === 'undefined' ) {
+
+			if ( 'ajaxurl' in PwCR ) {
+				ajax_url = PwCR.ajaxurl;
+//console.debug( 'from object' );
+			} else {
+				ajax_url = guessed_ajaxurl;
+//console.debug( 'from guessed url' );
+			}
+			
+		} else {
+//console.debug( 'from original var' );
+			ajax_url = ajaxurl;
+		}
+		
+//console.debug( ajax_url );
 		$( '#pwcr_ignore_nag' ).live(
 			'click',
 			function() {
-				$.post( ajaxurl, { 'action' : 'ignore_nag' },
+				
+				$.post( ajax_url, { 'action' : 'ignore_nag' },
 					
 					function( result ){
 						$( '#pwcr_nag' ).hide( 'slow' );
